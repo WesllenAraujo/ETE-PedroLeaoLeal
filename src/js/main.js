bc1e99c0b96630a030da71cf7f808f3fe0cfc48c
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-08-19 14:34:19 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,3 @@
-/* Objetivo: Pegar o momento que o usuário clique no botão de cursos no menu e mostre na tela a gama de cursos oferecidos pela ETE.
-
-PASSO 1° Pegar o botão
-PASSO 2° Atribuir o botão a uma variável
-
-PASSO 2° Adicionar um evento de click no botão
-PASSO 3° Criar uma função não determinada
-
-PASSO 4° Atriuir o menu de cursos a uma variável
-PASSO 5° Atrribuir a imagem do arrow a uma variável
-
-Passo 6° Criar uma estrutura condicional com if que se o menu de cursos estiver sendo exibido na tela, ele deixara oculto, senão ele vai deixar visível.
-PASSO 7° mudar a imagem do arrow no botão
-*/
-
 const courseButton = document.getElementById('coursesButton');
 
 courseButton.addEventListener('click', function() {

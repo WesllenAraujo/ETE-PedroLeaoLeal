c36b994229545bf3fe7b6929b2dc701e92fd1bc4
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-12-17 17:34:19 -0300

    Added the project code's root

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,7 @@
-function variables() {
-    const courseButton = document.getElementById('coursesButton');
-    let coursesList = document.getElementById('courses');
-    let arrowImage = document.querySelector('.navigation-arrow');
+let slideButtonsList = document.querySelectorAll("input[type=radio]");
+let i = 0;
 
-    return { courseButton, coursesList, arrowImage };
-}
-var { courseButton, coursesList, arrowImage } = variables();
-coursesList.style.display = 'none';
-courseButton.addEventListener('click', function() {
-    var { coursesList, arrowImage } = variables();
-    if (coursesList.style.display === 'none') {
-        coursesList.style.display = 'flex';
-        arrowImage.classList.remove('arrow-down');
-        arrowImage.classList.add('arrow-up');
-    } else {
-        coursesList.style.display = 'none';
-        arrowImage.classList.remove('arrow-up');
-        arrowImage.classList.add('arrow-down');
-    }
-});
+setInterval(() => {
+    slideButtonsList[i].checked = true;
+    i = (i + 1) % slideButtonsList.length;
+}, 5000);
